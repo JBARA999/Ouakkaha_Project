ef6966f2269694208f81e8470efec4fb4af53a27
omarchaibb <148008716+omarchaibb@users.noreply.github.com>
2025-03-20 15:44:37 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Ouakkaha from "./pages/Ouakkaha";
+import Contact from "./pages/Contact";
+import Alfissen from "./pages/Alfissen";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-      welcom
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ouakkaha" element={<Ouakkaha />} />
+        <Route path="/alaf-issen" element={<Alfissen />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,101 @@
-import React from 'react'
-
+import {
+  Facebook,
+  Headset,
+  Instagram,
+  Mail,
+  Phone,
+  Twitter,
+  Youtube,
+} from "lucide-react";
+import React from "react";
+import "../styles/footer.css";
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <div>
+        <span>OUAKKAHA MOHAMED</span>
+        <ul>
+          <li>
+            <Phone />
+            <span>+212 6 61 18 64 16</span>
+          </li>
+          <li>
+            <Phone />
+            <span>+212 6 61 20 95 16</span>
+          </li>
+          <li>
+            <Headset />
+
+            <span>+212 5 28 21 68 65</span>
+          </li>
+          <li>
+            <Headset />
+
+            <span>+212 5 28 21 62 62</span>
+          </li>
+          <li>
+            <Mail />
+            <span> oukkaha.med1@gmail.com</span>{" "}
+          </li>
+          <li>
+            <Mail />
+            <span>livraisonouk@gmail.com</span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <span>ALF ISSEN</span>
+        <ul>
+          <li>
+            <Phone />
+            <span>+212 6 61 18 64 16</span>
+          </li>
+          <li>
+            <Phone />
+            <span>+212 6 61 20 95 16</span>
+          </li>
+          <li>
+            <Headset />
+            <span>+212 5 28 21 68 65</span>
+          </li>
+          <li>
+            <Headset />
+            <span>+212 5 28 21 62 62</span>
+          </li>
+          <li>
+            <Mail />
+            <span>oukkaha.med1@gmail.com</span>
+          </li>
+          <li>
+            <Mail />
+            <span>service.commercial.alfissen@gmail.com</span>
+          </li>
+        </ul>
+      </div>
+      <div className="suiver-address">
+        <div className="suiver">
+          <span>Suivez-Nous</span>
+          <div />
+          <div className="social-media">
+            <div style={{ display: "flex", gap: "1rem" }}>
+              <Facebook />
+
+              <Twitter />
+              <Instagram />
+              <Youtube />
+            </div>
+          </div>
+        </div>
+        <div className="address">
+            <span>Adress</span>
+          <p>
+            Zaouiate Issen, Commune Rurale Eddir-Ouled Teima, Taroudant - Maroc
+          </p>
+
+          <p>B.P. N° 617 Hay Mohamadi Ouled Teima, Taroudant - Maroc</p>
+        </div>
+      </div>
+      <p>Société Nouvelle de Volaille | Tous droits réservés © 2025</p>
+    </footer>
+  );
 }
