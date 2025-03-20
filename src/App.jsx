91e2834043118578c4cf2ca246a02795f5922c07
omarchaibb <148008716+omarchaibb@users.noreply.github.com>
2025-03-20 15:49:21 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
@@ -7,16 +6,10 @@ import Contact from "./pages/Contact";
 import Alfissen from "./pages/Alfissen";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-=======
-import { Route, Router } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
->>>>>>> 4bbc495ceb0da0139b8ea7bb09817b7f9bf89eaa
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -25,9 +18,6 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
-=======
-     <Header/>
->>>>>>> 4bbc495ceb0da0139b8ea7bb09817b7f9bf89eaa
     </>
   );
 }
