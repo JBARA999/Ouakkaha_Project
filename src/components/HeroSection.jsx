d45d67fab8ffd693f414ab4826aaf75bde35c2c5
omarchaibb <148008716+omarchaibb@users.noreply.github.com>
2025-03-20 16:51:43 +0100

    j

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/hero.css"
+export default function HeroSection() {
+  return (
+    <div className="hero" style={{ backgroundImage: "ur" }}>
+      <div>
+        <h1>Notre Metier Est De preparer L'avenir</h1>
+        <p>
+          Nous sommes spécialisés dans la fourniture de solutions d’alimentation
+          de haute qualité pour les bovins, les ovins et les vaches laitières au
+          Maroc
+        </p>
+       <button >Voir plus</button>
+      </div>
+    </div>
+  );
+}
